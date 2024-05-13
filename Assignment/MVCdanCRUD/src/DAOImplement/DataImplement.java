ae6606db2bed368e9cb2a21092d2363dfd04ec63
AdhiKrisna <krisnahmbtn@gmail.com>
2024-05-13 14:17:42 +0700

    Assignment 2: CRUD Database MYSQL with MVC

--- Assignment/MVCdanCRUD/src/DAOImplement/DataImplement.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface DataImplement {
-    public void insert(DataMovie dm);
-    public void update(DataMovie dm);
-    public void delete(DataMovie dm);
-    public List<DataMovie> getAll();
-}
--- Assignment/MVCdanCRUD/src/view/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MainView frame = new MainView();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-    }
-}
